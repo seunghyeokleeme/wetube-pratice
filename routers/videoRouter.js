b31734a0e7f868f5734fb3dda436550105940a08
seunghyeokleeme <seunghyeok.tech@gmail.com>
2019-09-01 20:05:49 +0900

    MVC Pattern part Three

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,19 @@
 import express from "express";
+import routes from "../routes";
+import {
+  videos,
+  upload,
+  videoDetail,
+  editVideo,
+  deleteVideo
+} from "../controllers/videoController";
 
 const viedoRouter = express.Router();
+
+viedoRouter.get(routes.videos, videos);
+viedoRouter.get(routes.upload, upload);
+viedoRouter.get(routes.videoDetail, videoDetail);
+viedoRouter.get(routes.editVideo, editVideo);
+viedoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default viedoRouter;

dd228e5eae9a52b2a692e9ba54e7e17722a871b0
seunghyeokleeme <seunghyeok.tech@gmail.com>
2019-09-03 13:13:03 +0900

    Home Controller part One

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 57332,
+    title: "Video awsome",
+    description: "This is something I love",
+    view: 204,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 1,
+      name: "devinan",
+      email: "devinan.hr@gmail.com"
+    }
+  },
+  {
+    id: 22222,
+    title: "Video super",
+    description: "This is something I love",
+    view: 204,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 1,
+      name: "devinan",
+      email: "devinan.hr@gmail.com"
+    }
+  },
+  {
+    id: 439493,
+    title: "Video nice",
+    description: "This is something I love",
+    view: 204,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 1,
+      name: "devinan",
+      email: "devinan.hr@gmail.com"
+    }
+  },
+  {
+    id: 111111,
+    title: "Video perfect",
+    description: "This is something I love",
+    view: 204,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 1,
+      name: "devinan",
+      email: "devinan.hr@gmail.com"
+    }
+  }
+];

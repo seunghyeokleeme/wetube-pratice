98206a566f04fa8bceb59e4a52db932e5bb09949
seunghyeokleeme <seunghyeok.tech@gmail.com>
2019-12-20 01:37:47 +0900

    #2.7 Express Core Middlewares part Two

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 const app = express();
 
 const PORT = 3000;
@@ -10,12 +14,11 @@ const handleHome = (req, res) => res.send("Hello from home");
 
 const handleProfile = (req, res) => res.send("Hello from Profile!");
 
-const betweenHome = (req, res, next) => {
-  console.log("Between");
-  next();
-};
-
-app.use(betweenHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 

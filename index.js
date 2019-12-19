8eceac41741c2fd7615a82b55101d292bd31c773
seunghyeokleeme <seunghyeok.tech@gmail.com>
2019-12-20 01:17:32 +0900

    #2.6 Express Core: Middlewares

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ const handleHome = (req, res) => res.send("Hello from home");
 
 const handleProfile = (req, res) => res.send("Hello from Profile!");
 
+const betweenHome = (req, res, next) => {
+  console.log("Between");
+  next();
+};
+
+app.use(betweenHome);
+
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);

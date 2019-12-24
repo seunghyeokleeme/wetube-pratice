792ad8b2bdb733874c4f20445a1bc47044601d64
seunghyeokleeme <seunghyeok.tech@gmail.com>
2019-12-24 21:15:30 +0900

    #2.8 Express Core Routing

--- app.js
+++ app.js
@@ -3,12 +3,8 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import { userRouter } from "./router";
 const app = express();
-
-const PORT = 3000;
-
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("Hello from home");
 
@@ -24,4 +20,6 @@ app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);
+
+export default app;

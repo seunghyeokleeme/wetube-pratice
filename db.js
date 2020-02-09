aa36e3fddad9f8babac26b956ca6a32d64107652
seunghyeokleeme <seunghyeok.tech@gmail.com>
2020-02-10 06:06:08 +0900

    Home Controller

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 324393,
+    title: "Video awsome",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Devinan",
+      email: "devi@nan.com"
+    }
+  },
+  {
+    id: 424234,
+    title: "Video super",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 43456,
+      name: "Devinan",
+      email: "devi@nan.com"
+    }
+  },
+  {
+    id: 78695,
+    title: "Video nice",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 753434,
+      name: "Devinan",
+      email: "devi@nan.com"
+    }
+  },
+  {
+    id: 64358,
+    title: "Video perfect",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 683457,
+      name: "Devinan",
+      email: "devi@nan.com"
+    }
+  }
+];

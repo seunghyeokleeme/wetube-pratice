525f1885ca3f84e98842e15d534ce50915521e91
seunghyeokleeme <seunghyeok.tech@gmail.com>
2020-02-09 19:36:07 +0900

    Handling Routes

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const app = express();
 
 const PORT = 4000;
 
+function handleHome(req, res) {
+  res.send("Home");
+}
+
+function handleProfile(req, res) {
+  res.send("profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 function handleListening() {
   console.log(`Listening on: http://localhost:${PORT}`);
 }
